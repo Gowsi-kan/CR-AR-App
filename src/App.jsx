850a1e5f431c4b822d8ff13d5e0937b94546b434
Gowsikan <gowsikan32412@gmail.com>
2023-06-06 20:01:46 +0530

    add 1,2,3

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 // Sample typing
+// 1,2,3
 function App() {
   const [count, setCount] = useState(0)
 
